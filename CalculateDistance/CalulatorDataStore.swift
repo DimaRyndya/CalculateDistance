eca73436793f43ac085f47c530cfaa5cbbefa8c5
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-10-09 20:09:57 +0300

    done

--- CalculateDistance/CalulatorDataStore.swift
+++ CalculateDistance/CalulatorDataStore.swift
@@ -29,21 +29,26 @@ final class CalculatorDataStore {
         guard let contentsURL = urlComponents.url else { return }
 
         URLSession.shared.dataTask(with: contentsURL) { data, response, error in
-                   if let data = data,
-                         let response = response as? HTTPURLResponse {
-                         print(response.statusCode)
-                       guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                             let respo = Response(from: json) else {
-                           return
-                       }
-                       print(respo.originAddress)
-                       print(respo.destinationAddress)
-                       print(respo.distance)
-                       print(respo.duration)
+            if let data = data,
+               let response = response as? HTTPURLResponse {
+                print(response.statusCode)
+                if let decodedResponse = try? JSONDecoder().decode(
+                    Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        print(decodedResponse.originAddress)
+                        print(decodedResponse.destinationAddress)
+                    }
                 }
-                print(
-                  "Contents fetch failed: " +
-                    "\(error?.localizedDescription ?? "Unknown error")")
+                guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                      let respo = Response(from: json) else {
+                    print(
+                        "Contents fetch failed: " +
+                        "\(error?.localizedDescription ?? "Unknown error")")
+                    return
+                }
+                print(respo.distance)
+                print(respo.duration)
+            }
         }
         .resume()
     }
--- CalculateDistance/Response.swift
+++ CalculateDistance/Response.swift
@@ -22,40 +22,17 @@ struct Response {
     }
 }
 
-//extension Response: Decodable {
-//
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        originAddress = try container.decode([String].self, forKey: .originAddress)
-//        destinationAddress = try container.decode([String].self, forKey: .destinationAddress)
-//        //        let elementsContainer = try container.nestedContainer(keyedBy: ElemKeys.self, forKey: .rows)
-//        //        let valuesContainer = try elementsContainer.nestedContainer(keyedBy: ValuesKeys.self, forKey: .elements)
-//        //        let distanceContainer = try valuesContainer.nestedContainer(keyedBy: DistanceKeys.self, forKey: .distance)
-//        //        distance = try distanceContainer.decode(String.self, forKey: .text)
-//        //        let durationContainer = try valuesContainer.nestedContainer(keyedBy: DurationKeys.self, forKey: .duration)
-//        //        duration = try durationContainer.decode(String.self, forKey: .text)
-//    }
-//}
-//
-//    enum CodingKeys: String, CodingKey {
-//        case originAddress = "origin_addresses"
-//        case destinationAddress = "destination_addresses"
-////        case rows
-//    }
-////
-////    enum ElemKeys: String, CodingKey {
-////        case elements
-////    }
-////
-////    enum ValuesKeys: String, CodingKey {
-////        case distance, duration
-////    }
-////
-////    enum DistanceKeys: String, CodingKey {
-////        case text
-////    }
-////
-////    enum DurationKeys: String, CodingKey {
-////        case text
-////    }
-////}
+extension Response: Decodable {
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        originAddress = try container.decode([String].self, forKey: .originAddress)
+        destinationAddress = try container.decode([String].self, forKey: .destinationAddress)
+    }
+}
+
+    enum CodingKeys: String, CodingKey {
+        case originAddress = "origin_addresses"
+        case destinationAddress = "destination_addresses"
+    }
+
